771982ee6a80aa77522e739ef44e6e81dc49f210
Nitish Kumar Mandal <147975324+Nitish-kr-Mandal@users.noreply.github.com>
2026-01-19 13:27:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let message = document.getElementById("status")
+let btn = document.getElementById("enterBtn")
+
+btn.addEventListener('click', () => {
+	message.innerHTML = "Entered Metaverse"
+})
